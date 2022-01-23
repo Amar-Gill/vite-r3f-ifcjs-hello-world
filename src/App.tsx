e578fca1a22325f97c1d72b293a12b9c42c226a5
Amar Gill <amardeep@clearbanc.com>
2022-01-23 14:20:07 -0500

    chore: set up canvas view with gridhelper

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
 import React from 'react';
 
 function App() {
-  return <div></div>;
+  return (
+    <Canvas>
+      <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
+      <gridHelper />
+    </Canvas>
+  );
 }
 
 export default App;
